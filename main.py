950d7ed1616d3400679068c60f79f34a75aa6ce6
Joaquin-Gael <guzmanantonio867@gmail.com>
2025-08-14 01:58:57 -0300

    save changes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+from utils import console
 import torch as th
 from torch import nn
 import torch.nn.functional as F
@@ -7,6 +9,8 @@ from torch.utils.tensorboard import SummaryWriter
 import numpy as np
 
 import pandas as pd
+
+import polars as pl
 
 import plotly.graph_objects as go
 
@@ -18,42 +22,81 @@ from rich.progress import (
     TimeElapsedColumn,
     TimeRemainingColumn
 )
-from rich import print
 
 from pathlib import Path
 
+from transformers import LogitsProcessorList, RepetitionPenaltyLogitsProcessor, TopPLogitsWarper
+
 import itertools
 
-from utils import get_data, decode_text, encode_text
+from utils import get_data, decode_text, encode_text, only_spanish_letters
 
-writer = SummaryWriter(
-    str(Path(__file__).parent.joinpath('logs').resolve()),
-)
+console.print(th.cuda.is_available(), style="bold green")
+console.print(th.cuda.get_device_name(0) if th.cuda.is_available() else "No CUDA available", style="bold green")
+
+device = th.device("cuda" if th.cuda.is_available() else "cpu")
+      
+#writer = SummaryWriter(
+ #  str(Path(__file__).parent.joinpath('logs').resolve()),
+#)
 
 class WikiDataset(Dataset):
     def __init__(self):
         super().__init__()
-        self.data = get_data()
+        self.data: Optional[pl.DataFrame] = get_data()
 
     def __len__(self):
+        if self.data is None:
+            console.print("Dataset is empty or not loaded.", style="bold red")
+            return 0
         return len(self.data)
 
     def shape(self):
+        if self.data is None:
+            console.print("Dataset is empty or not loaded.", style="bold red")
+            return (0, 0)
         return self.data.shape
 
     def __getitem__(self, idx):
-        return (th.tensor(self.data.select("word")\
-            .to_numpy())[idx],
-                th.tensor(self.data.select("prediction")\
-                          .to_numpy())[idx])
+        if self.data is None:
+            console.print("Dataset is empty or not loaded.", style="bold red")
+            raise RuntimeError("Dataset is empty or not loaded.")
+        # Extraer valores (polars permite indexar la columna como lista-like)
+        t0 = int(self.data["token0"][idx])
+        t1 = int(self.data["token1"][idx])
+
+        p0 = self.data["pred0"][idx]
+
+        # Definir pad/ignore id (usa -100 para CrossEntropy ignore_index)
+        pad_id = getattr(self, "pad_token_id", -100)
+
+        p0 = int(p0) if p0 is not None else pad_id
+
+        # Tensores de entrada y target
+        x = th.tensor([t0, t1], dtype=th.long)   # shape (2,)
+        y = th.tensor([p0], dtype=th.long)   # shape (2,)
+
+        return x, y
 
     @property
     def words(self):
-        plane_words = list(itertools.chain(*self.data.select("word").to_numpy().tolist()))
-        return plane_words
+        if self.data is None:
+            console.print("Dataset is empty or not loaded.", style="bold red")
+            return []
+        
+        list_words = list(itertools.chain(*self.data.select("token0").to_numpy().tolist()))
+        list_words.extend(list(itertools.chain(*self.data.select("token1").to_numpy().tolist())))
+        
+        console.print(f"Typo de list_words: {type(list_words)}", style="bold green")
+        console.print(f"Total de palabras únicas: {len(set(list_words))}", style="bold green")
+        
+        return list_words
 
     @property
     def labels(self):
+        if self.data is None:
+            console.print("Dataset is empty or not loaded.", style="bold red")
+            return []
         plane_words = list(itertools.chain(*self.data.select("prediction").to_numpy().tolist()))
         return plane_words
 
@@ -127,6 +170,7 @@ class WordRRN_LSTM(nn.Module):
         self.embedding = nn.Embedding(vocab_size, emb_size, padding_idx=pad_token)
         self.rnn = nn.RNN(emb_size, hidden_size, num_layers=3, batch_first=True)
         self.lstm = nn.LSTM(hidden_size, hidden_size, num_layers=5, batch_first=True)
+        self.conv = nn.Conv1d(hidden_size, hidden_size, kernel_size=1, padding=0)
         self.dropout = nn.Dropout(p=0.3)
         self.fc = nn.Linear(hidden_size, output_size)
         self.softmax = nn.Softmax(dim=1)
@@ -135,16 +179,55 @@ class WordRRN_LSTM(nn.Module):
         emb = self.embedding(input_words)
         rnn_out, _ = self.rnn(emb)
         dropout = self.dropout(rnn_out)
+        cov_out = self.conv(dropout.permute(0, 2, 1))  # Conv1d expects (batch_size, channels, seq_len)
+        cov_out = cov_out.permute(0, 2, 1)  #
         lstm_out, _ = self.lstm(dropout)
         h_pool = th.mean(lstm_out, dim=1)
         x = self.fc(h_pool)
         out = self.softmax(x)
         return out
 
+class TransformerModel(nn.Module):
+    def __init__(self, vocab_size, emb_size, hidden_size, output_size, pad_token=0):
+        super(TransformerModel, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, emb_size, padding_idx=pad_token)
+        self.layer_norm = nn.LayerNorm(emb_size)
+        self.multy_head_attention = nn.MultiheadAttention(emb_size, num_heads=8, batch_first=True, dropout=0.3)
+        self.FFN = nn.Sequential(
+            nn.Linear(emb_size, hidden_size),
+            nn.GELU(),
+            nn.Linear(hidden_size, emb_size),
+            nn.Dropout(p=0.2)
+        )
+        self.out_put = nn.Sequential(
+            nn.Linear(emb_size, output_size),
+            nn.Softmax(dim=-1)
+        )
+        
+    def block_model(self, emb):
+        norm = self.layer_norm(emb)
+        attention_out, _ = self.multy_head_attention(norm, norm, norm, need_weights=False)
+        residual = attention_out + emb
+        norm_out = self.layer_norm(residual)
+        ffn_out = self.FFN(norm_out)
+        return ffn_out + norm_out
+    
+    def forward(self, input_words):
+        emb = self.embedding(input_words)
+        block_out = self.block_model(emb)
+        block_out = self.block_model(block_out)
+        block_out = self.block_model(block_out)
+        block_out = self.block_model(block_out)
+        norm_ffn_out = self.layer_norm(block_out)
+        out = self.out_put(norm_ffn_out)
+        return out
 
 dataset = WikiDataset()
 
-train_dataset, test_dataset = random_split(dataset, [int(0.8*len(dataset)), len(dataset) - int(0.8*len(dataset))])
+train_dataset, test_dataset = random_split(dataset, [int(0.85*len(dataset)), len(dataset) - int(0.85*len(dataset))])
+
+train_dataset: th.Tensor = train_dataset
+test_dataset: th.Tensor = test_dataset
 
 train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)
@@ -155,11 +238,12 @@ output_size = vocab_size
 hidden_size = 256
 num_steps = 3
 
-num_epochs = 1
+num_epochs = 300
 
-model = WordRRN_LSTM(vocab_size, emb_size, hidden_size, output_size, pad_token=0)
+model = TransformerModel(vocab_size, emb_size, hidden_size, output_size, pad_token=0)
+model = model.to(device)
 criterion = nn.CrossEntropyLoss()
-optimizer = th.optim.Adam(model.parameters(), lr=0.001)
+optimizer = th.optim.Adam(model.parameters(), lr=0.000003)
 
 if __name__ == "__main__":
     print(f"Size Data Set: {dataset.shape()}")
@@ -168,17 +252,22 @@ if __name__ == "__main__":
     print(f"Output Size: {output_size}")
     print(f"Input Size: {emb_size}")
     print(f"Number of Epochs: {num_epochs}")
-    print(f"Word Size: {test_dataset[1][0].item()}")
-    print(f"Word Decode: {decode_text([test_dataset[1][0].item()])}")
+    print(f"Word Size: {test_dataset[1][0].tolist()}")
+    print(f"Word Decode: {decode_text(test_dataset[1][0].tolist())}")
 
     with th.inference_mode():
         sequence_input = "Argentina es"
         encode = encode_text(sequence_input)
         input_tensor = th.tensor([encode], dtype=th.long)
+        
+        input_tensor = input_tensor.to(device)
 
         out = model(input_tensor)
 
-        predicted_indices = out.argmax(dim=1).tolist()
+        predicted_indices = out.argmax(dim=-1).tolist()[0]
+        console.print("Predicted Indices:", predicted_indices, style="bold green")
+        console.print("out.shape:", out.shape)       # debe ser (batch, seq_len, vocab) si todo está bien
+        console.print("out.min(), out.max():", out.min().item(), out.max().item())
         sequence_output = sequence_input + decode_text(predicted_indices)
 
         print("Oración final:", sequence_output)
@@ -212,7 +301,13 @@ if __name__ == "__main__":
                 progress.update(task, info=f"Lote {idx}")
 
                 input_words = batch[0]
+                #console.print("Input Words:", input_words, style="bold green")
+                #console.print("Input Words Shape:", input_words.shape, style="bold green")
                 target = batch[1]
+                #console.print("Target:", target, style="bold green")
+                #console.print("Target Shape:", target.shape, style="bold green")
+                input_words = input_words.to(device)
+                target = target.to(device)
                 try:
                     out = model(input_words)
                 except Exception as e:
@@ -220,9 +315,9 @@ if __name__ == "__main__":
                     print("Word:", input_words)
                     continue
 
-                loss = criterion(out, target.squeeze(dim=1))
+                loss = criterion(out[:, 0], target[:, 0])
                 progress.update(task, loss=f"{loss.item():.4f}")
-                writer.add_scalar("train loss", loss.item(), idx)
+                #writer.add_scalar("train loss", loss.item(), idx)
                 total_loss_train.append(loss.item())
                 progress.update(task, loss_mean=f"{sum(total_loss_train) / len(total_loss_train)}")
                 loss.backward()
@@ -258,6 +353,8 @@ if __name__ == "__main__":
 
                 input_words = batch[0]
                 target = batch[1]
+                input_words = input_words.to(device)
+                target = target.to(device)
 
                 try:
                     out = model(input_words)
@@ -266,28 +363,37 @@ if __name__ == "__main__":
                     print("Word:", input_words)
                     continue
 
-                loss = criterion(out, target.squeeze(dim=1))
+                loss = criterion(out[:, 0], target[:, 0])
                 progress.update(task, loss=f"{loss.item():.4f}")
-                writer.add_scalar("test loss", loss.item(), idx)
+                #writer.add_scalar("test loss", loss.item(), idx)
                 total_loss_test.append(loss.item())
                 progress.update(task, loss_mean=f"{sum(total_loss_test) / len(total_loss_test)}")
-                loss.backward()
-                optimizer.step()
-                optimizer.zero_grad()
+                #loss.backward()
+                #optimizer.step()
+                #optimizer.zero_grad()
                 progress.update(task ,completed=float(idx))
+            
+            #traced = th.jit.trace(
+            #    lambda X: model(X),
+            #    th.tensor(
+            #        [encode_text("Argentina es")],
+            #        dtype=th.long
+            #    ).to(device),
+            #    strict=False
+            #)
 
         print(f"Epoch {epoch}, Error: {sum(total_loss_test) / len(total_loss_test)}")
 
 
-        writer.add_graph(
-            model,
-            th.tensor(
-                [encode_text("Argentina es")],
-                dtype=th.long
-            )
-        )
+        #writer.add_graph(
+         #   traced,
+         #   th.tensor(
+         #       [encode_text("Argentina es")],
+          #      dtype=th.long
+          #  ).to(device)
+        #)
 
-        writer.close()
+        #writer.close()
 
         diff = len(total_loss_test) - len(total_loss_train)
 
@@ -342,10 +448,52 @@ if __name__ == "__main__":
         sequence_input = "Argentina es"
         encode = encode_text(sequence_input)
         input_tensor = th.tensor([encode], dtype=th.long)
+        
+        out_put_ids: list[int] = []
+        
+        input_tensor = input_tensor.to(device)
 
         out = model(input_tensor)
 
-        predicted_indices = out.argmax(dim=1).tolist()
+        predicted_indices = out.argmax(dim=-1).tolist()[0]
+        out_put_ids.extend(predicted_indices)
         sequence_output = sequence_input + decode_text(predicted_indices)
+        
+        for i in range(10):
+            out = model(th.tensor([encode_text(sequence_output)], dtype=th.long).to(device))
+            
+            last_logits = out[:, -1, :]
+            
+            processor = LogitsProcessorList([
+                RepetitionPenaltyLogitsProcessor(penalty=1.2)
+            ])
+            
+            warped_logits = processor(input_ids=input_tensor, scores=last_logits)
+            
+            warper = TopPLogitsWarper(top_p=0.9)
+            warped_logits = warper(input_tensor, warped_logits)
+            
+            probs = th.softmax(warped_logits, dim=-1)
+            next_id = th.multinomial(probs, num_samples=1)
+            
+            out_put_ids.append(next_id.item())
+            
+            input_tensor = th.cat([input_tensor, next_id], dim=1)
+            
+        console.print("Out Put Ids:", out_put_ids)
 
-        print("Oración final:", sequence_output)
+        decode_data = decode_text(out_put_ids)
+        
+        console.print("Decode Ids:", decode_data)
+
+        console.print("Original Sequence:", sequence_input + decode_data)
+            
+        console.print("Final Sequence only spanish:", only_spanish_letters(sequence_input + decode_data))
+        
+    if df_loss["Loss Train"].isnull().all():
+        console.print("No hay datos de pérdida de entrenamiento para mostrar.", style="bold red")
+        
+    elif df_loss["Loss Test"].mean() < 5:
+        console.print("El modelo ha convergido con éxito.", style="bold green")
+        th.save(model.state_dict(), "model.pth")
+        console.print("Modelo guardado como 'model.pth'.", style="bold green")
--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import tiktoken
 import requests as rq
 from bs4 import BeautifulSoup
 
+import re
+import unicodedata
 
 import polars as pl
 
@@ -19,18 +21,126 @@ enc = tiktoken.encoding_for_model("gpt-4o")
 
 console = rich.console.Console()
 
+SPANISH_LETTERS_PATTERN = re.compile(r"[^A-Za-zÁÉÍÓÚÜáéíóúüÑñ\s]+$")
+#RE_NON_LATIN = re.compile(r"[^\p{Latin}\s]+")
+
+def is_spanish_word(text: str) -> str | None:
+    if not SPANISH_LETTERS_PATTERN.search(text):
+        return text
+    else:
+        return ""
+
+def only_spanish_letters(text: str) -> str | None:
+    text = unicodedata.normalize("NFC", text)
+
+    out_chars = []
+    for ch in text:
+        if ch.isspace():
+            out_chars.append(" ")
+            continue
+        cat = unicodedata.category(ch)
+        if cat.startswith("L"):
+            try:
+                name = unicodedata.name(ch)
+            except ValueError:
+                out_chars.append(" ")
+                continue
+            if "LATIN" in name:
+                out_chars.append(ch)
+                continue
+        out_chars.append(" ")
+
+    cleaned = "".join(out_chars).strip()
+
+    #cleaned = RE_NON_LATIN.sub(" ", cleaned)
+
+    cleaned = SPANISH_LETTERS_PATTERN.sub(" ", cleaned)
+    cleaned = re.sub(r"\s+", " ", cleaned).strip()
+    word = is_spanish_word(cleaned)
+
+    return word
+
 
 def get_data() -> pl.DataFrame | None:
     try:
         html_wiki = rq.get("https://es.wikipedia.org/wiki/Argentina").text
 
-        soup = BeautifulSoup(html_wiki, features="lxml")
+        python_sub_html = [
+            "",
+            "appetite.html",
+            "interpreter.html",
+            "interpreter.html#invoking-the-interpreter",
+            "interpreter.html#argument-passing",
+            "interpreter.html#interactive-mode",
+            "interpreter.html#the-interpreter-and-its-environment",
+            "interpreter.html#source-code-encoding",
+            "introduction.html",
+            "introduction.html#using-python-as-a-calculator"
+        ]
 
-        list_words = soup.get_text().splitlines()
+        soup = BeautifulSoup(html_wiki, features="lxml")
+        soup = soup.get_text().splitlines()
+        for sub_html in python_sub_html:
+            html_python_docs_es = rq.get("https://docs.python.org/es/3/tutorial/"+sub_html).text
+            soup.extend(BeautifulSoup(html_python_docs_es, features="lxml").get_text().splitlines())
+
+        html_python_wiki = rq.get("https://es.wikipedia.org/wiki/Python").text
+
+        soup.extend(BeautifulSoup(html_python_wiki, features="lxml").get_text().splitlines())
+
+        more_html_data = [
+            "https://es.wikipedia.org/wiki/Albert_Einstein",
+            "https://es.wikipedia.org/wiki/Germán_Garmendia",
+            "https://espanol.lingolia.com/es/ayuda/glosario",
+            "https://es.wikipedia.org/wiki/Miguel_de_Cervantes",
+            "https://es.wikipedia.org/wiki/Don_Quijote_de_la_Mancha",
+            "https://es.wikipedia.org/wiki/Pablo_Neruda",
+            "https://es.wikipedia.org/wiki/Las_Meninas",
+            "https://es.wikipedia.org/wiki/La_Gioconda",
+            "https://es.wikipedia.org/wiki/Johann_Sebastian_Bach",
+            "https://es.wikipedia.org/wiki/Mozart",
+            "https://es.wikipedia.org/wiki/Leonhard_Euler",
+            "https://es.wikipedia.org/wiki/Inteligencia_artificial",
+            "https://es.wikipedia.org/wiki/Poder_ejecutivo",
+            "https://es.wikipedia.org/wiki/Carne",
+            "https://es.wikipedia.org/wiki/Historia_del_pan",
+            "https://es.wikipedia.org/wiki/Batalla_de_Hastings",
+            "https://es.wikipedia.org/wiki/Batalla_de_Cannas",
+            "https://es.wikipedia.org/wiki/Primera_guerra_mundial",
+            "https://es.wikipedia.org/wiki/Christopher_Columbus"
+        ]
+
+        for more_html in more_html_data:
+            html_more_data = rq.get(more_html).text
+            soup.extend(BeautifulSoup(html_more_data, features="lxml").get_text().splitlines())
+
+        console.print("Longitud de la sopa de texto: ", len(soup))
+
+        list_words = soup
+
+        spanish_words = []
+
+        for word in list_words:
+            word = only_spanish_letters(word)
+            if word:
+                spanish_words.append(word)
+
+        list_words = spanish_words
+
 
         couple_words = {
-            "word":[],
-            "prediction":[],
+            "token0": [],
+            "token1": [],
+            "token2": [],
+            "token3": [],
+            "token4": [],
+            "token5": [],
+            "token6": [],
+            "token7": [],
+            "token8": [],
+            #"token9": [],
+            #"token10": [],
+            "pred0": [],
         }
         with Progress(
                 SpinnerColumn(),
@@ -42,32 +152,95 @@ def get_data() -> pl.DataFrame | None:
 
             task = progress.add_task("Procesando líneas...", total=len(list_words), step="Inicializando")
 
+            sequence_buffer, len_buffer = [], 0
+
             for line in list_words:
-                progress.update(task, step="Codificando línea")
+                progress.update(task, step="Codificando línea") 
                 encoded = enc.encode(line)
+
+                if len(encoded)-9 < 0:
+                    len_buffer += 1
+                    sequence_buffer.extend(encoded)
+                    continue
+
+                if len_buffer > 1:
+                    encoded.extend(sequence_buffer)
+                    sequence_buffer = []
                 couple = {
-                    "word": [],
-                    "prediction": [],
+                    "token0": [],
+                    "token1": [],
+                    "token2": [],
+                    "token3": [],
+                    "token4": [],
+                    "token5": [],
+                    "token6": [],
+                    "token7": [],
+                    "token8": [],
+                    #"token9": [],
+                    #"token10": [],
+                    "pred0": [],
                 }
 
-                for i, word in enumerate(encoded):
-                    progress.update(task, step=f"Procesando palabra {i+1} de {len(encoded)}")
-                    couple["word"].append(word)
-                    if i + 1 < len(encoded):
-                        couple["prediction"].append(encoded[i+1])
-                    else:
-                        couple["prediction"].append(enc.encode(" ")[0])
+                for i in range(len(encoded)-9):
+                    
+                    t0 = encoded[i]
+                    t1 = encoded[i+1]
+                    t2 = encoded[i+2]
+                    t3 = encoded[i+3]
+                    t4 = encoded[i+4]
+                    t5 = encoded[i+5]
+                    t6 = encoded[i+6]
+                    t7 = encoded[i+7]
+                    t8 = encoded[i+8]
+                    #t9 = encoded[i+9]
+                    #t10 = encoded[i+10]
+                    preds = encoded[i+9] if i+9 < len(encoded) else 0
 
-                couple_words["word"].extend(couple["word"])
-                couple_words["prediction"].extend(couple["prediction"])
+                    p0 = int(preds)
+
+                    couple["token0"].append(t0)
+                    couple["token1"].append(t1)
+                    couple["token2"].append(t2)
+                    couple["token3"].append(t3)
+                    couple["token4"].append(t4)
+                    couple["token5"].append(t5)
+                    couple["token6"].append(t6)
+                    couple["token7"].append(t7)
+                    couple["token8"].append(t8)
+                    #couple["token9"].append(t9)
+                    #couple["token10"].append(t10)
+                    couple["pred0"].append(p0)
+
+                couple_words["token0"].extend(couple["token0"])
+                couple_words["token1"].extend(couple["token1"])
+                couple_words["token2"].extend(couple["token2"])
+                couple_words["token3"].extend(couple["token3"])
+                couple_words["token4"].extend(couple["token4"])
+                couple_words["token5"].extend(couple["token5"])
+                couple_words["token6"].extend(couple["token6"])
+                couple_words["token7"].extend(couple["token7"])
+                couple_words["token8"].extend(couple["token8"])
+                #couple_words["token9"].extend(couple["token9"])
+                #couple_words["token10"].extend(couple["token10"])
+                couple_words["pred0"].extend(couple["pred0"])
+                
 
                 progress.advance(task)
+
+
+                
+                
+        console.print("Datos procesados correctamente.", style="bold green")
+        console.print(f"Total de palabras procesadas: {couple_words['token0'][:10]}")
+        console.print(f"Total de predicciones procesadas: {couple_words['pred0'][:10]}")
 
         df = pl.DataFrame(couple_words)
 
         return df
     except Exception as e:
-        console.print_exception(show_locals=True)
+        console.print_exception(show_locals=False)
+        print(f"Error al obtener los datos: {e}")
+        console.print("No se pudo obtener el DataFrame.", style="bold red")
         return None
 
 def encode_text(text: str) -> list[int]:
